b2ef764e8a261b0a9abee45f7046d905b3541692
takumi777-beep <takumi.yoshi.0413@gmail.com>
2026-06-28 21:58:47 +0900

    first commit

---
+++ db/migrate/20260627144511_add_userld_to_post.rb
@@ -0,0 +1,4 @@
+class AddUserldToPost < ActiveRecord::Migration[8.1]
+  def change
+  end
+end
---
+++ db/migrate/20260628052127_add_userld_to_posts.rb
@@ -0,0 +1,5 @@
+class AddUserldToPosts < ActiveRecord::Migration[8.1]
+  def change
+    add_column :posts, :user_id, :integer
+  end
+end
